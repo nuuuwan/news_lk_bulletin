1bf8f4194b2e2e423879d0cd324e78b9e80e0baf
nuuuwan <nuuuwan@gmail.com>
2024-02-10 16:02:56 +0530

    workflows

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,11 +1,10 @@
-import os
-
+import sys
 from news_lk_bulletin import NewsBulletin
 from utils_future import LLM
 
 
 def main():
-    openai_api_key = os.environ['OPENAI_API_KEY']
+    openai_api_key = sys.argv[1]
     llm = LLM(openai_api_key)
     NewsBulletin(llm=llm, max_articles=100, max_data_bytes=35_000).write()
 
